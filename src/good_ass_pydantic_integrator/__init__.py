6c04fcd8ca4cf34a051fb8841aa0cacb10f9a924
ryn-cx <262888154+ryn-cx@users.noreply.github.com>
2026-02-21 12:17:24 -0800

    Simplified exports

--- src/good_ass_pydantic_integrator/__init__.py
+++ src/good_ass_pydantic_integrator/__init__.py
@@ -1,17 +1,7 @@
 """Good Ass Pydantic Integrator - Utilities for integrating Pydantic models."""
 
-from good_ass_pydantic_integrator.customizer import (
-    CustomSerializer,
-    ReplacementField,
-    ReplacementType,
-)
-from good_ass_pydantic_integrator.gapi import GAPI
-from good_ass_pydantic_integrator.gapi_client import GAPIClient
-
-__all__ = [
-    "GAPI",
-    "CustomSerializer",
-    "GAPIClient",
-    "ReplacementField",
-    "ReplacementType",
-]
+from good_ass_pydantic_integrator.customizer import CustomSerializer as CustomSerializer
+from good_ass_pydantic_integrator.customizer import ReplacementField as ReplacementField
+from good_ass_pydantic_integrator.customizer import ReplacementType as ReplacementType
+from good_ass_pydantic_integrator.gapi import GAPI as GAPI
+from good_ass_pydantic_integrator.gapi_client import GAPIClient as GAPIClient
